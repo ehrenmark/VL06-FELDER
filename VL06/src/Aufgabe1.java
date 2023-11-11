d383db724fd79738c9304c12a8dac312d1126c5e
Mark Kagermeier <95859677+ehrenmark@users.noreply.github.com>
2023-11-11 10:50:00 +0100

    first commit

---
+++ VL06/src/Aufgabe1.java
@@ -0,0 +1,24 @@
+
+
+//a) Heap-Speicher
+//b) Jedes Element wird zu Anfang false sein.
+
+
+//c)
+public class Aufgabe1 {
+    public static void main(String[] args) {
+            // Annahme: Ein 2D-Feld mit dem Namen "zweidimensionalesFeld"
+                    int[][] zweidimensionalesFeld = {
+                            {1, 2, 3},
+                            {4, 5, 6},
+                            {7, 8, 9}
+                    };
+
+            // Bestimmen Sie die Länge der 3. Zeile
+                    int laengeDerDrittenZeile = zweidimensionalesFeld[2].length;
+
+            // Ausgabe der Länge
+                    System.out.println("Länge der 3. Zeile: " + laengeDerDrittenZeile);
+
+    }
+}
